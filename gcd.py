c351eadd431a450bb2eb28f0b35cb04e56548ca1
mukesh shelke <67261625+mukesh2309@users.noreply.github.com>
2021-10-21 19:42:39 +0530

    created a program to find gcd

---
+++ gcd.py
@@ -0,0 +1,16 @@
+# Recursive function to return gcd of a and b
+def gcd(a,b):
+	
+	# Everything divides 0
+	if (b == 0):
+		return a
+	return gcd(b, a%b)
+
+# Driver program to test above function
+a = 98
+b = 56
+if(gcd(a, b)):
+	print('GCD of', a, 'and', b, 'is', gcd(a, b))
+else:
+	print('not found')
+
